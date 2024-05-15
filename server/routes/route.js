04e57de364ac40a05433445f2521b8e5a2f330f3
Vishal <vishalverma1273@gmail.com>
2024-05-15 12:41:17 +0530

    post is done

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,18 @@
 import express from "express";
 
 import { signupUser, loginUser } from "../controller/user-controller.js";
+// import { uploadImage } from "../controller/image-controller.js";
+import { uploadImage, getImage } from "../controller/image-controller.js";
+import upload from "../utils/upload.js";
+import CreatePost from "../../client/src/components/create/CreatePost.jsx";
+import { authorization } from "../controller/jwt-controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/file/upload", upload.single("file"), uploadImage);
+router.get("/file/:filename", getImage);
+router.post("/create", CreatePost);
 
 export default router;
